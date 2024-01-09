b2a8fd6f287823df9c8543ee2f1556d867ca6951
Luca Joos <1a85ra7z@gmail.com>
2024-01-09 15:24:16 +0100

    feat: add buttons

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from '../components/Link';
+import {AtSign, GitHub, Music} from 'react-feather';
+import Image from 'next/image';
 
 const Index = () => {
   return (
@@ -11,11 +13,24 @@ const Index = () => {
               <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           </Head>
           <div className={'w-screen bg-black h-screen'}>
-              <Link href={'https://snapshot.lucajoos.de/privacy.html'} className={'fixed left-5 bottom-5 text-xs text-[#2A2A2A]'}>Datenschutzerklärung & Impressum</Link>
+              <div className={'flex flex-col items-center justify-center w-screen h-screen'}>
+                  <img src={'./assets/01/logo.png'} alt='Luca Joos' className={'z-10 w-1/3 max-w-[800px] max-h-[600px] min-w-[250px]' }></img>
+                  <div className={'flex gap-5'}>
+                      <Link href={'https://github.com/lucajoos'}>
+                          <GitHub />
+                      </Link>
 
-              <div className={'flex flex-col items-center justify-center gap-10 w-screen h-screen'}>
-                  <img src={'/../assets/01/logo.png'} alt='Luca Joos' className={'z-10 w-1/3 max-w-[800px] max-h-[600px] min-w-[250px] -translate-y-2'}/>
+                      <Link href={'https://li.sten.to/Mn6MT47'}>
+                          <Music />
+                      </Link>
+
+                      <Link href={'mailto:hello@lucajoos.de'}>
+                          <AtSign />
+                      </Link>
+                  </div>
               </div>
+
+              <Link href={'https://snapshot.lucajoos.de/privacy.html'} className={'fixed left-5 bottom-5 text-xs text-[#2A2A2A]'}>Datenschutzerklärung & Impressum</Link>
 
               <div className={'opacity-5 absolute top-0 left-0 w-screen bg-repeat bg-black h-screen pointer-events-none'} style={{ backgroundImage: "url('./assets/01/perlin.png')" }}></div>
           </div>
